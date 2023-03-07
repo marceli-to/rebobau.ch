a0639a4d47679386c5e593ff7770b7fe6ec900bd
Marcel Stadelmann <m@marceli.to>
2023-03-07 14:42:53 +0100

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Tasks\SetFilePermissions;
 
 class Kernel extends ConsoleKernel
 {
@@ -23,7 +22,6 @@ class Kernel extends ConsoleKernel
    */
   protected function schedule(Schedule $schedule)
   {
-    // $schedule->call(new SetFilePermissions)->everyMinute();
   }
 
   /**
